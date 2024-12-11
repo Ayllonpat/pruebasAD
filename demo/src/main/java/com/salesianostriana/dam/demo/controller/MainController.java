732ee396467657b5ed386d8600929eb840e5a4c8
Patricia Alonso Ayllón <alonso.aypat22@salesianos.triana.edu>
2024-12-11 10:59:33 +0100

    .

--- demo/src/main/java/com/salesianostriana/dam/demo/controller/MainController.java
+++ demo/src/main/java/com/salesianostriana/dam/demo/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.salesianostriana.dam.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,15 +12,16 @@ public class MainController {
         return "Hello world";
     }
 
-    @GetMapping("/producto")
+    @GetMapping("/producto/123")
     public Producto getProducto(){
         return new Producto(123L, "un producto molon");
     }
 
-
-
-    record Producto (Long id, String nombre){
-
+    @PostMapping("/producto")
+    public Producto nuevoProducto(@RequestBody Producto producto){
+        return producto;
     }
+
+    record Producto (Long id, String nombre){}
 }
 
